4cb87b6adefe4ee568d2e0316d49e5495e4f88c7
KalebJReyes <kalebreyes.business@gmail.com>
2026-01-13 00:45:36 -0800

    Finished Minigame & Devlog

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,47 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        // Moves player left/right
+        if (Input.GetKey("a"))
+        {
+            _playerTransform.Translate(Vector3.left * Time.deltaTime * _speed);
+        }
+        else if (Input.GetKey("d")) 
+        {
+            _playerTransform.Translate(Vector3.right * Time.deltaTime * _speed);
+        }
+        // Moves player up/down
+        if (Input.GetKey("w"))
+        {
+            _playerTransform.Translate(Vector3.up * Time.deltaTime * _speed);
+        }
+        else if (Input.GetKey("s"))
+        {
+            _playerTransform.Translate(Vector3.down * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKeyDown("space")) 
+        {
+            PlantSeed();
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0) 
+        { 
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/MjLLqDcN)
 # HW1
 ## Devlog
-Write your Devlog here!
+Kaleb Reyes, he/him
+
+The plan my table made and my code have the Player be able to move use WASD and spawning seeds using space. The number of seeds in my code is tracked using three different variables rather than the one that my plan mentioned. The UI in my code doesn't have a variable to track the amount of seeds left and planted. Rather, it uses the variables in the Player script. Like mentioned in the plan, the UI gets updated everytime the user presses the space bar.
+
 
 
 ## Open-Source Assets
